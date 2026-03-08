7066bbad505339a0d4ea9c883c1908c66d450631
Aditya Singh <adityasingh01517@gmail.com>
2026-03-08 11:20:00 +0530

    feat: implement expense parsing node

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import date, datetime
 from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
 import calendar
-from prompts import intent_prompt_template
+from prompts import intent_prompt_template, expense_prompt_template
 from dotenv import load_dotenv
 import os
 import json
@@ -58,6 +58,23 @@ def intent_classification_node(state: AppState):
     parsed_data = structured_llm.invoke(prompt)
     print(parsed_data)
     return {"intent": parsed_data.intent}
+
+
+def parse_expense_node(state: AppState):
+    expense_prompt = PromptTemplate(
+        input_variables=["user_input", "datetimes", "day"],
+        template=expense_prompt_template,
+    )
+    user_message = state["user_query"]
+    prompt = expense_prompt.format(
+        user_input=user_message,
+        datetimes=datetime.today(),
+        day=calendar.day_name[date.today().weekday()],
+    )
+    structured_llm = light_llm.with_structured_output(Expenses)
+    parsed_data = structured_llm.invoke(prompt)
+    print(parsed_data)
+    return {"expenses": parsed_data.expenses, "new_expenses": parsed_data.expenses}
 
 
 if __name__ == "__main__":
